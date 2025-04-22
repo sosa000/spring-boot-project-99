24de86808373083d0787997780bbe2d900587ea7
soso <dannykulikov123@gmail.com>
2025-04-22 15:39:20 +0300

    Init Spring project

--- src/main/java/hexlet/code/app/controller/WelcomeController.java
+++ src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -1,2 +1,16 @@
-package hexlet.code.app.controller;public class WelcomeController {
+package hexlet.code.app.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping(path = "/welcome")
+    @ResponseStatus(HttpStatus.OK)
+    public String index() {
+        return "Welcome to Spring";
+    }
 }
